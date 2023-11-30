25175cbf14efc98fb8eee1ebba21691267f60341
Rahtu Anom <rahtuanom10@gmail.com>
2023-11-30 20:41:50 +0800

    30/11
    Perampungan Index,About us

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,58 @@ done!
 */
 
 console.log('javascript ready...')
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
 
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".room-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
 //! Responsive Navigation BAR 🎉
 const showMobileNav = () => {
   var navbar = document.getElementById("navbar");
